3f2ba7ba609c4a56ce38668bea9d12de52c0732b
Cody Wong <codyjwong@gmail.com>
2026-01-20 17:11:06 -0800

    Update welcome message in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 const welcomeStrings = [
-  "Hello Express!",
+  "Hello Express! 123",
   "To learn more about Express on Vercel, visit https://vercel.com/docs/frameworks/backend/express",
 ]
 
